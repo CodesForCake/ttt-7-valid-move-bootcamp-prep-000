3c43ad6323f6badec013d50f7d9952896c7f3342
Mimi C <mina123fire@yahoo.com>
2020-08-28 19:25:01 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,3 +5,10 @@ def valid_move?(input)
   end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, input)
+  if(board[input]==""||board[input]==" "||board[input]==nil)
+    return FALSE
+  elsif (board[input]=="X"||board[input]=="O")
+    return TRUE
+  end
+end
