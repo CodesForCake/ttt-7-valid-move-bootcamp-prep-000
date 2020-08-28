ae239e80d9dbb44c7ed24f4c32852c6042cc0365
Mimi C <mina123fire@yahoo.com>
2020-08-28 19:25:11 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,6 +3,7 @@ def valid_move?(input)
   if input<0 || input >8
     return FALSE
   end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, input)
